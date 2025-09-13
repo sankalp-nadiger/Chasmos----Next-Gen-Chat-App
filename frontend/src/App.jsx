057031fa087e78f5c7fa36aaa9cf8d747fb28cb4
unknown <nadigersankalp@gmail.com>
2025-09-13 22:16:22 +0530

    initial setup commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white text-4xl">
+      🚀 Tailwind 3 + Vite + React working!
+    </div>
+  )
+}
