11c91dd437762a184e9f3e29f8afe647362f78d7
Sankalp Nadiger <128134398+sankalp-nadiger@users.noreply.github.com>
2025-10-30 19:48:08 +0530

    Chat-request icon spell bug

--- frontend/src/components/ChattingPage.jsx
+++ frontend/src/components/ChattingPage.jsx
@@ -7,7 +7,7 @@ import React, {
 } from "react";
 
 import { motion, AnimatePresence } from "framer-motion";
-import chatReqIcon from '../assets/chat-reuest.png';
+import chatReqIcon from '../assets/Chat-reuest.png';
 import chatAcceptIcon from '../assets/chat-accepted.png';
 import {
   Search,
